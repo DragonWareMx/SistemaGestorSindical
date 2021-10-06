3f9e6e213f305a19aa2bf9ddfbd8c391529cedef
PapaDrago <oscarwaii@hotmail.com>
2021-10-05 19:39:04 -0500

    Inertia
    ya está  listo el inertia de nada

---
+++ resources/js/Pages/Ejemplo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Welcome = () => {
+    return (
+        <div>
+            <h1>Welcome</h1>
+            <p style={{ fontFamily: "Atma" }}>Hello welcome to your first Inertia app!</p>
+        </div >
+    )
+}
+
+export default Welcome
