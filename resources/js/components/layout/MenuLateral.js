faae493cd5498850042f5b80d6dc5119302dec24
leoDlopez <lopez_lopez_daniel@hotmail.com>
2021-11-01 13:16:04 -0600

    Algunos cambios

--- resources/js/components/layout/MenuLateral.js
+++ resources/js/components/layout/MenuLateral.js
@@ -75,7 +75,7 @@ export default function MenuLateral() {
               <InertiaLink href={route('perfil')} className="icono-menu">
                 <div className="menu-profile-info">
                   <img style={{ marginLeft: '10px' }} src={auth.user.foto ? "/storage/fotos_perfil/" + auth.user.foto : "/img/avatar1.png"} className="main-userimage" />
-                  <div className="profile-info-name truncate">{auth.user.employee.nombre + ' ' + auth.user.employee.apellido_p + ' ' + auth.user.employee.apellido_m}</div>
+                  <div className="profile-info-name truncate">{auth.user.employee.apellido_m ? auth.user.employee.nombre + ' ' + auth.user.employee.apellido_p + ' ' + auth.user.employee.apellido_m : auth.user.employee.nombre + ' ' + auth.user.employee.apellido_p}</div>
                 </div>
               </InertiaLink>
             </div>
